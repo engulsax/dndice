cbab9dd6548d11f5c477a2a4d4145332023f35eb
EnGulSax <adamcarlleo@gmail.com>
2021-12-12 14:50:29 +0100

    deploy

---
+++ precache-manifest.121f76daec235e055ee52ab22f833e9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99d44cdf8791a3ba2a93",
+    "url": "/css/app.9e7a33c1.css"
+  },
+  {
+    "revision": "96852c96368bfc815c9c4412cb7a4458",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99d44cdf8791a3ba2a93",
+    "url": "/js/app.5989aace.js"
+  },
+  {
+    "revision": "e5ae5e840d031858e090",
+    "url": "/js/chunk-vendors.a548650a.js"
+  },
+  {
+    "revision": "0f99edd6bdf22d835617",
+    "url": "/js/icons.df52a9fc.js"
+  },
+  {
+    "revision": "b9722714f28abb5219d252164cd06d81",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
