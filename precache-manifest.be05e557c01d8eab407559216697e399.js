02d4fe3e596fa8f1af4c756709e99aefeeabbe7b
EnGulSax <adamcarlleo@gmail.com>
2022-02-05 18:51:50 +0100

    deploy

---
+++ precache-manifest.be05e557c01d8eab407559216697e399.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eee1c417879567703b9b85a68442a032",
+    "url": ".well-known/assetlinks.json"
+  },
+  {
+    "revision": "05390f0488e4194fb82c",
+    "url": "css/app.f2f99f3d.css"
+  },
+  {
+    "revision": "aa3356b35a7fff398315",
+    "url": "css/home.ffa49818.css"
+  },
+  {
+    "revision": "ec91e063ed3e1d3bb037",
+    "url": "css/privacy.eb2f0cf2.css"
+  },
+  {
+    "revision": "79c401ddee853207866246b96d13c637",
+    "url": "index.html"
+  },
+  {
+    "revision": "05390f0488e4194fb82c",
+    "url": "js/app.fa6233e9.js"
+  },
+  {
+    "revision": "86797f781f2527894c69",
+    "url": "js/chunk-vendors.e4a9934c.js"
+  },
+  {
+    "revision": "aa3356b35a7fff398315",
+    "url": "js/home.c089e43e.js"
+  },
+  {
+    "revision": "d6a0eb64a9db49a7382d",
+    "url": "js/icons.a2cc463b.js"
+  },
+  {
+    "revision": "ec91e063ed3e1d3bb037",
+    "url": "js/privacy.6608b8b7.js"
+  },
+  {
+    "revision": "b9722714f28abb5219d252164cd06d81",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
